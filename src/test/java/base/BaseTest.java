c121163875037eb89a097fd5d5b2caec0fc3a83e
narasimha123gopal-dev <narasimha123gopal@gmail.com>
2026-04-20 00:29:49 +0530

    Fixed CI headless Chrome issue

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -24,7 +24,11 @@ public class BaseTest {
         options.addArguments("--disable-notifications");
         options.addArguments("--disable-save-password-bubble");
         options.addArguments("--disable-extensions");
-
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
